0598855603d5cc516ead6274f4d3c507f44ceaa4
Faqihasyari <faqih8158@gmail.com>
2025-04-21 22:58:10 +0700

    generate code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_db/app/data/db/note_database.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(NotesAdapter());
   runApp(
     GetMaterialApp(
       title: "Application",
