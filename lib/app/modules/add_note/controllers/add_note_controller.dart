c0d91d179cb3f8df49bc2dfa67577f5c9393c7dc
Faqihasyari <faqih8158@gmail.com>
2025-04-22 23:22:54 +0700

    add note 4

--- lib/app/modules/add_note/controllers/add_note_controller.dart
+++ lib/app/modules/add_note/controllers/add_note_controller.dart
@@ -8,13 +8,26 @@ class AddNoteController extends GetxController {
   TextEditingController descC = TextEditingController();
 
   void addNote() async {
-    Notes note = Notes()
-    ..id = 0
-      ..title = titleC.text
-      ..desc = descC.text;
+    if (titleC.text.isNotEmpty && descC.text.isNotEmpty) {
+      //Membuat Tanpa ID
+      Notes note = Notes()
+        ..title = titleC.text
+        ..desc = descC.text;
 
       final box = NoteManager.getAllNotes();
+
+      //DAPET ID AUTO INCREMENT
       int id = await box.add(note);
+      note.id = id;
+      await note.save();
+
+      //EDIT NOTE => DENGAN ID YANG KITA DAPAT
       print("ID : $id");
+      print("BERHASIL MENAMBAHKAN DATA KE BOX (LOCA STORAGE DENGAN HIVE)");
+
+      print(box.get(id)?.id);
+      print(box.get(id)?.title);
+      print(box.get(id)?.desc);
+    }
   }
 }
